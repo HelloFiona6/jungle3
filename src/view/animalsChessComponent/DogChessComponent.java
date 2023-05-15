a808c4722b79362febe9cc65857ddafd8093d0ae
HelloFiona6 <1870341385@qq.com>
2023-05-15 11:41:10 +0800

    初始化棋盘和棋子图标

--- src/view/animalsChessComponent/DogChessComponent.java
+++ src/view/animalsChessComponent/DogChessComponent.java
@@ -14,6 +14,7 @@ public class DogChessComponent extends JComponent {
     private PlayerColor owner;
 
     private boolean selected;
+    private ImageIcon image;
 
     public DogChessComponent(PlayerColor owner, int size) {
         this.owner = owner;
@@ -37,13 +38,7 @@ public class DogChessComponent extends JComponent {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        Font font = new Font("楷体", Font.PLAIN, getWidth() / 2);
-        g2.setFont(font);
-        g2.setColor(owner.getColor());
-        g2.drawString("狗", getWidth() / 4, getHeight() * 5 / 8); // FIXME: Use library to find the correct offset.
-        if (isSelected()) { // Highlights the model if selected.
-            g.setColor(Color.RED);
-            g.drawOval(0, 0, getWidth() , getHeight());
-        }
+        image=new ImageIcon("resource\\ChessImage\\Dog.png");
+        g.drawImage(image.getImage(),0,0,getWidth(),getHeight(), this);
     }
 }
